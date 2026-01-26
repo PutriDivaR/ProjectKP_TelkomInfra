be1d4ea98a4a2aa8c8fc8a6a08e945db18130cc5
Putri Diva Riyanti <putridivariyanti@gmail.com>
2026-01-26 14:33:11 +0700

    bot v1 (awal)

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Halo! Proyek Node.js pertamaku berhasil jalan.');
 });
 
-app.listen(port, () => {
-  console.log(`Aplikasi berjalan di http://localhost:${port}`);
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
--- src/app.js
+++ src/app.js
@@ -5,9 +5,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+// routes
 const dashboardRoutes = require('./routes/dashboard.routes');
 app.use('/', dashboardRoutes);
+
+// bot API routes 
+const botRoutes = require('./routes/bot.routes');
+app.use('/api/bot', botRoutes);
 
 module.exports = app;
